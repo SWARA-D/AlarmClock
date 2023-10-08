cee8d7e68d2288e3a356d900692ce79376fa4241
SWARA-D <91810485+SWARA-D@users.noreply.github.com>
2023-10-08 16:18:52 +0530

    Create SetTimeActivity.kt

---
+++ java/com.practice.alarmclock/SetTimeActivity.kt
@@ -0,0 +1,87 @@
+package com.practice.alarmclock
+
+import android.app.*
+import android.content.Context
+import android.content.Intent
+import android.os.Bundle
+import android.os.PersistableBundle
+import android.view.ViewGroup
+import android.widget.Button
+import android.widget.TextView
+import android.widget.TimePicker
+import android.widget.Toast
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.app.NotificationCompat
+import androidx.core.content.ContextCompat
+import com.practice.alarmclock.databinding.ActivityMainBinding
+import com.practice.alarmclock.databinding.SetTimeActivityBinding
+import java.util.*
+import com.google.android.material.datepicker.MaterialDatePicker.Builder.datePicker
+
+
+
+
+class SetTimeActivity : AppCompatActivity() {
+
+    var clockHour = 0
+    var clockMinute = 0
+    var clockAm_Pm = ""
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.set_time_activity)
+
+
+        val timePicker = findViewById<TimePicker>(R.id.timePicker)
+        timePicker.setOnTimeChangedListener { _, hour, minute ->
+            var hour = hour
+            var am_pm = ""
+
+            when {
+                hour == 0 -> {
+                    hour += 12
+                    am_pm = "AM"
+                }
+                hour == 12 -> am_pm = "PM"
+                hour > 12 -> {
+                    hour -= 12
+                    am_pm = "PM"
+                }
+                else -> am_pm = "AM"
+
+            }
+
+            clockHour = hour
+            clockMinute = timePicker.minute
+            clockAm_Pm = am_pm
+        }
+
+            val setAlarmBtn = findViewById<Button>(R.id.setAlarmBtn)
+            setAlarmBtn.setOnClickListener {
+
+                val calendar: Calendar = Calendar.getInstance().apply {
+                    timeInMillis = System.currentTimeMillis()
+                    set(Calendar.HOUR_OF_DAY, timePicker.hour)
+                    set(Calendar.MINUTE, timePicker.minute)
+                }
+
+
+                scheduleNotification(this,calendar.timeInMillis,"Alarm","Don't Forget")
+
+            }
+
+
+    }
+    fun scheduleNotification(context: Context, time: Long, title: String, text: String) {
+        val alarmintent = Intent(context, AlarmReceiver::class.java)
+        alarmintent.putExtra("title", title)
+        alarmintent.putExtra("text", text)
+
+        val pending =
+            PendingIntent.getBroadcast(context, 42, alarmintent, PendingIntent.FLAG_UPDATE_CURRENT)
+
+        val manager = context.getSystemService(Context.ALARM_SERVICE) as AlarmManager
+        manager.setExactAndAllowWhileIdle(AlarmManager.RTC_WAKEUP, time, pending)
+    }
+
+}
